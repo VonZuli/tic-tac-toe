a8d715c3aa66db6b1fe5063714da61f8a3440084
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-09-07 16:43:22 -0400

    created html markup and styling

---
+++ src/script.js
@@ -0,0 +1,23 @@
+'use strict';
+console.log("Connected")
+
+let game = {
+  gameboard: ['❌','⭕'],
+
+  players:{
+    p1:{
+      //player info
+    },
+    p2:{
+      //player info
+    }
+  },
+
+  logic:{
+    //game flow logic
+  }
+}
+
+function start(params) {
+  
+}
